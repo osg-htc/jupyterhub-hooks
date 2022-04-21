d1b250ff0964981e9cb84dfe370e2eb7979c064e
Brian Aydemir <baydemir@morgridge.org>
2022-04-21 07:27:42 -0500

    Poll for sssd's socket only when not running as "jovyan"

--- containers/minimal-notebook/opt/osg/bin/entrypoint.sh
+++ containers/minimal-notebook/opt/osg/bin/entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
-# Wait for sssd to create its sockets.
+# Prepare the host before starting the notebook server.
 
 set -eu
 
-_target=/var/lib/sss/pipes/nss
+## Wait for sssd to create its sockets when not running as "jovyan".
 
-until test -e "${_target}"
-do
-  printf 'Waiting for %s (does not exist)\n' "${_target}"
-  sleep 2
-done
+_sssd_socket=/var/lib/sss/pipes/nss
+
+if [ "$(id -u)" != "1000" ]; then
+  until test -e "${_sssd_socket}"; do
+    printf 'Waiting for %s to be created\n' "${_sssd_socket}"
+    sleep 2
+  done
+fi
 
 exec "$@"
